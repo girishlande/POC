9a4922a040ebc36a08530a936da0e6ba539c131e
AGILiAD\Girish.Lande <girish.lande@agiliad.com>
2019-10-18 16:39:40 +0530

    Added project

---
+++ C#BackendQMLFrontEndEntityFrameworkDemo/EmployeeServer/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using EmployeeDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        EmployeeDbContext ctx = new EmployeeDbContext();
+
+        public IEnumerable<Employee> Get()
+        {
+            return ctx.Employees;
+        }
+
+        public Employee Get(int id)
+        {
+            foreach(var v in ctx.Employees)
+            {
+                if (v.EmpId == id)
+                    return v;
+            }
+            return new Employee() { EmpId = -1, EmpName = "NA", EmpCity = "NA" };
+        }
+
+        public void Post(Employee emp)
+        {
+            ctx.Employees.Add(emp);
+            ctx.SaveChanges();
+        }
+
+        public void Put(int id,Employee emp)
+        {
+            Employee e = ctx.Employees.Find(id);
+            if (e != null)
+            {
+                e.EmpCity = emp.EmpCity;
+                e.EmpName = emp.EmpName;
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            Employee e = ctx.Employees.Find(id);
+            if (e !=null)
+            {
+                ctx.Employees.Remove(e);
+                ctx.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ C#BackendQMLFrontEndEntityFrameworkDemo/EmployeeServer/Models/EmployeeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDemo.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext()
+            :base()
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
